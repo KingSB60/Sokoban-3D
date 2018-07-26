b47aa927c27690ac8f4332f503148823b9fccaf3
KingSB60 <sb_ga@onlinehome.de>
2018-07-26 23:32:12 +0200

    V 0.2.001
    Buttons auf LevelCompleted richtig verlinkt

---
+++ Assets/LevelCompletedButtons.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCompletedButtons : MonoBehaviour
+{
+    private GameObject mainMenuCanvas;
+    private GameObject selectorCanvas;
+    private GameObject completedCanvas;
+
+    private void Awake()
+    {
+        mainMenuCanvas = Utils.FindIncludingInactive("MainMenuCanvas");
+        selectorCanvas = Utils.FindIncludingInactive("LevelSelectorCanvas");
+        completedCanvas = Utils.FindIncludingInactive("CompletedCanvas");
+    }
+
+    public void GoToLevelSelector()
+    {
+        completedCanvas.GetComponent<Canvas>().enabled = false;
+        selectorCanvas.GetComponent<Canvas>().enabled = true;
+    }
+    public void GoToMainMenu()
+    {
+        completedCanvas.GetComponent<Canvas>().enabled = false;
+        mainMenuCanvas.GetComponent<Canvas>().enabled = true;
+    }
+}
